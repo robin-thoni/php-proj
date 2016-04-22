dfd8062f35277862ab76ee1995b846dee5906d1c
Robin THONI <robin@rthoni.com>
2016-04-22 15:22:38 +0200

    pick file

--- public/app/controllers/upload.controller.js
+++ public/app/controllers/upload.controller.js
@@ -4,11 +4,30 @@ angular.module('app')
 
             $scope.image = null;
 
+            $scope.input = angular.element(angular.element("#filePicker")[0]);
+            $scope.input.on("change", function (e) {
+                var reader = new FileReader();
+                var f = e.target.files[0];
+                reader.readAsBinaryString(f);
+                reader.onload = (function(file) {
+                    return function(e) {
+                        $scope.$apply(function() {
+                            $scope.fileDropped(e.target.result, file);
+                        });
+                    };
+                })(f);
+            });
+
             $scope.fileDropped = function(content, file)
             {
                 $scope.image = {
                     content: content,
                     name: file.name
                 };
+            };
+
+            $scope.pickFile = function()
+            {
+                $scope.input.click();
             };
     }]);
