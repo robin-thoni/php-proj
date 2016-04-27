dee0281c4f6445cf5fe8ff262d26fc9ccf23cf38
Robin THONI <robin@rthoni.com>
2016-04-27 20:24:55 +0200

    order by images results

--- sql/struct_init.sql
+++ sql/struct_init.sql
@@ -43,6 +43,7 @@ CREATE FUNCTION sp_get_images(_only_mine boolean, _user_id_me integer, OUT id in
         SELECT i.id, i.name, i.user_id, i.date
         FROM images i
         WHERE (CASE WHEN _only_mine THEN i.user_id = _user_id_me ELSE TRUE END)
+        ORDER BY i.date DESC
 $$;
 
 
